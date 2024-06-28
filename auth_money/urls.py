7ab9edef3988c70d6998e794a8885c101f34b3fa
huihyeon06 <agnea0829@gmail.com>
2024-06-28 10:41:42 +0900

    Create, Update 구현

--- auth_money/urls.py
+++ auth_money/urls.py
@@ -2,9 +2,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'auth_money'
+
 urlpatterns = [
     path('', views.main_money_view, name='main_money'),
     path('login/', views.login_view, name='login'),
     path('add_money/', views.add_money_view, name='add_money'),
-    path('contents_list/', views.ContentsList.as_view(), name="contents_list"),
+    path('content_list/', views.ContentsList.as_view(), name="content_list"),
+    path('content_create/', views.ContentsCreate.as_view(), name="content_create"),
+    path('content_update/<int:pk>/', views.ContentsUpdate.as_view(), name="content_update"),
+    path('content_delete/<int:pk>/', views.ContentsDelete.as_view(), name="content_delete"),
 ]
